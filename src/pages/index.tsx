b56996d608fe079684a968cf1e3f029f7a16e1ab
Lucas César <lucasambrogi1@hotmail.com>
2021-03-01 01:09:08 -0300

    Home page: implemented the components.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
+import Head from 'next/head';
+
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { Countdown } from "../components/Countdown";
+import { Profile } from "../components/Profile";
+
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
+
       <ExperienceBar />
+
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
